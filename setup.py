9e3b08ecb17a1cff39ee21eade580fb6c4583f5a
Alberto Berti <alberto@metapensiero.it>
2016-02-22 16:16:00 +0100

    add package installation stuff

---
+++ setup.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+# :Project:   raccoon.rocky.node -- A base object for publishing WAMP resources
+# :Created:   dom 09 ago 2015 12:57:35 CEST
+# :Author:    Alberto Berti <alberto@arstecnica.it>
+# :License:   GNU General Public License version 3 or later
+# :Copyright: Copyright (C) 2016 Arstecnica s.r.l.
+#
+
+import os
+from codecs import open
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.rst'), encoding='utf-8') as f:
+    CHANGES = f.read()
+with open(os.path.join(here, 'version.txt'), encoding='utf-8') as f:
+    VERSION = f.read().strip()
+
+setup(
+    name="pyxc",
+    version=VERSION,
+    url="https://github.com/azazel75/pyxc-pj",
+
+    description="A Python 3 converter to ES6 Javascript",
+    long_description=README + u'\n\n' + CHANGES,
+
+    author="Alberto Berti",
+    author_email="alberto@arstecnica.it",
+
+    license="GPLv3+",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        ],
+    keywords='',
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=[],
+
+    install_requires=['setuptools'],
+    extras_require={'dev': ['metapensiero.tool.bump_version', 'docutils']},
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest',],
+)
