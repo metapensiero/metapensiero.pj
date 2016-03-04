c4456d407cd2be1160e0d619514047f90db7ed16
Alberto Berti <alberto@metapensiero.it>
2016-03-04 17:00:14 +0100

    simplify test values

--- tests/test_ast.py
+++ tests/test_ast.py
@@ -16,27 +16,29 @@ def test_ast_func(astdump):
 
     node, dump = astdump(func)
 
-    expected = ''.join('AsyncFunctionDef(args=arguments(args=[], \n'
-                       '                                defaults=[], \n'
-                       '                                kw_defaults=[], \n'
-                       '                                kwarg=None, \n'
-                       '                                kwonlyargs=[], \n'
-                       '                                vararg=None), \n'
-                       "                 body=[Import(names=[alias(asname='aio', \n"
-                       "                                           name='asyncio')]), \n"
-                       '                       Assign(targets=[Name(ctx=Store(), \n'
-                       "                                            id='a')], \n"
-                       "                              value=BinOp(left=Str(s='abc'), \n"
-                       '                                          op=Mult(), \n'
-                       '                                          right=Num(n=3))), \n'
-                       '                       Assign(targets=[Name(ctx=Store(), \n'
-                       "                                            id='b')], \n"
-                       '                              value=BinOp(left=Num(n=2), \n'
-                       '                                          op=Pow(), \n'
-                       '                                          right=Num(n=3)))], \n'
-                       '                 decorator_list=[], \n'
-                       "                 name='func', \n"
-                       '                 returns=None)')
+    expected = (
+        'AsyncFunctionDef(args=arguments(args=[], \n'
+        '                                defaults=[], \n'
+        '                                kw_defaults=[], \n'
+        '                                kwarg=None, \n'
+        '                                kwonlyargs=[], \n'
+        '                                vararg=None), \n'
+        "                 body=[Import(names=[alias(asname='aio', \n"
+        "                                           name='asyncio')]), \n"
+        '                       Assign(targets=[Name(ctx=Store(), \n'
+        "                                            id='a')], \n"
+        "                              value=BinOp(left=Str(s='abc'), \n"
+        '                                          op=Mult(), \n'
+        '                                          right=Num(n=3))), \n'
+        '                       Assign(targets=[Name(ctx=Store(), \n'
+        "                                            id='b')], \n"
+        '                              value=BinOp(left=Num(n=2), \n'
+        '                                          op=Pow(), \n'
+        '                                          right=Num(n=3)))], \n'
+        '                 decorator_list=[], \n'
+        "                 name='func', \n"
+        '                 returns=None)'
+    )
     assert dump == expected
 
 
@@ -52,7 +54,7 @@ def test_ast_class_super(astjs):
         def meth(self):
             super().another_meth(x, y)
 
-    expected = ''.join((
+    expected = (
         'class A {\n'
         '    constructor(value) {\n'
         '        var d;\n'
@@ -63,7 +65,7 @@ def test_ast_class_super(astjs):
         '    meth() {\n'
         '        super.another_meth(x, y);\n'
         '    }\n'
-        '}\n')
+        '}\n'
     )
 
     assert str(astjs(A)) == expected
