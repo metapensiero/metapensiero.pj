28d94e9304d0b26d0e7677680b6093a6289baacb
Alberto Berti <alberto@metapensiero.it>
2016-03-04 17:09:18 +0100

    Add a test on imports

--- tests/test_various.py
+++ tests/test_various.py
@@ -5,6 +5,9 @@
 # :License:   GNU General Public License version 3 or later
 #
 
+from pj.api import translate_object
+
+
 def test_body_names_stop_at_func(astobj):
 
     from pj.processor.util import body_local_names
@@ -28,3 +31,24 @@ def test_textwrap_behavior():
     import textwrap
     out = textwrap.dedent(txt)
     assert len(out) == 16
+
+
+def test_imports():
+
+    def func():
+        import foo, bar
+        import foo.bar as b
+        from foo.bar import hello as h, bye as bb
+        from ..foo.zoo import bar
+        from . import foo
+        from .foo import bar
+
+    expected = ("import * as foo from 'foo';\n"
+                "import * as bar from 'bar';\n"
+                "import * as b from 'foo/bar';\n"
+                "import {hello as h, bye as bb} from 'foo/bar';\n"
+                "import {bar} from '../../foo/zoo';\n"
+                "import * as foo from './foo';\n"
+                "import {bar} from './foo';\n")
+
+    assert translate_object(func, body_only=True, enable_es6=True)[0] == expected
