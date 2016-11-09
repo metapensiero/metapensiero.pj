3864029bf1a8c6c90934de88cb3b5e49af9041bf
Alberto Berti <alberto@metapensiero.it>
2016-11-10 00:44:52 +0100

    Move testing utils to their own module

---
+++ src/metapensiero/pj/testing.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# :Project: pyxc-pj -- test utilities
+# :Created: mer 09 nov 2016 20:32:09 CET
+# :Author:  Alberto Berti <alberto@metapensiero.it>
+# :License: GNU General Public License version 3 or later
+#
+
+import ast
+import inspect
+import textwrap
+
+from . import transformations
+from .js_ast import JSStatements
+from .processor.transforming import Transformer
+
+
+def ast_object(obj):
+    """Convert a Python object to its ast representation."""
+    src = inspect.getsource(obj)
+    # ast wraps always into a module
+    node = ast.parse(textwrap.dedent(src)).body[0]
+    return node
+
+
+def ast_dump_object(obj, first_stmt_only=False):
+    """Convert a Python object to its ast representation and the serialization of
+    this to a string, optionally serializing the first element in its body.
+    """
+    from meta.asttools import str_ast
+    node = ast_object(obj)
+    if first_stmt_only:
+        node = node.body[0]
+    return node, str_ast(node)
+
+
+def ast_object_to_js(obj, es6=False):
+    """Convert a Python object to JS using pj, optionally transforming with ES6
+    features enabled.
+    """
+    src = inspect.getsource(obj)
+    node = ast.parse(textwrap.dedent(src))
+    t = Transformer(transformations, JSStatements, es6=es6)
+    return t.transform_code(node)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,45 +1,22 @@
 # -*- coding: utf-8 -*-
-# :Project:  pyxc-pj -- test fixtures
-# :Created:    lun 22 feb 2016 12:16:42 CET
-# :Author:    Alberto Berti <alberto@metapensiero.it>
-# :License:   GNU General Public License version 3 or later
+# :Project: pyxc-pj -- test fixtures
+# :Created: lun 22 feb 2016 12:16:42 CET
+# :Author:  Alberto Berti <alberto@metapensiero.it>
+# :License: GNU General Public License version 3 or later
 #
-import ast
-import inspect
-import textwrap
 
 import pytest
-
-from metapensiero.pj.processor.transforming import Transformer
-from metapensiero.pj import transformations
-from metapensiero.pj.js_ast import JSStatements
-
-def ast_object(obj):
-    src = inspect.getsource(obj)
-    # ast wraps always into a module
-    node = ast.parse(textwrap.dedent(src)).body[0]
-    return node
-
-def ast_dump_object(obj, first_stmt_only=False):
-    from meta.asttools import str_ast
-    node = ast_object(obj)
-    if first_stmt_only:
-        node = node.body[0]
-    return node, str_ast(node)
-
-def ast_object_to_js(obj, es6=False):
-    src = inspect.getsource(obj)
-    node = ast.parse(textwrap.dedent(src))
-    t = Transformer(transformations, JSStatements, es6=es6)
-    return t.transform_code(node)
+from metapensiero.pj.testing import ast_object, ast_dump_object, ast_object_to_js
 
 @pytest.fixture
 def astdump():
     return ast_dump_object
 
+
 @pytest.fixture
 def astobj():
     return ast_object
+
 
 @pytest.fixture
 def astjs():
