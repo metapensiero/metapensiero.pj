11016f6e7194b5f119a9d3f3f99e6650624300da
Alberto Berti <alberto@metapensiero.it>
2017-03-15 15:04:20 +0100

    Update authors

--- src/metapensiero/pj/transformations/obvious.py
+++ src/metapensiero/pj/transformations/obvious.py
@@ -2,7 +2,8 @@
 # :Project:  metapensiero.pj -- simpler transformations
 # :Created:  ven 26 feb 2016 15:17:49 CET
 # :Authors:  Andrew Schaaf <andrew@andrewschaaf.com>,
-#            Alberto Berti <alberto@metapensiero.it>
+#            Alberto Berti <alberto@metapensiero.it>,
+#            Lele Gaifax <lele@metapensiero.it>
 # :License:  GNU General Public License version 3 or later
 #
 
