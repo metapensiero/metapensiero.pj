687efe2ae15a3b10637e2dd92186cc6e38f70a94
Lele Gaifax <lele@metapensiero.it>
2017-03-19 11:21:11 +0100

    Revert special support for elif statement
    This is a partial revert of commit d40b0dce8318a22270e96f6433434b211bceebca:
    it produced incorrectly indented code, not worth the hassle for now.

--- tests/test_various/test_translate_object/else_if.js
+++ tests/test_various/test_translate_object/else_if.js
@@ -2,8 +2,10 @@ var a;
 a = 1;
 if ((a === 0)) {
     window.alert("Can't be");
-} else if ((a < 0)) {
-    window.alert("Something's wrong");
 } else {
-    window.alert("That's it");
+    if ((a < 0)) {
+        window.alert("Something's wrong");
+    } else {
+        window.alert("That's it");
+    }
 }
