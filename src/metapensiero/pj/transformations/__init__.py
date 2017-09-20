4e00d3b6a400cb216a07ddf6d80393a5057490c9
Alberto Berti <alberto@metapensiero.it>
2017-09-20 17:12:43 +0200

    Raise an error instead of a warning in case of dict keys of wrong type

--- src/metapensiero/pj/transformations/__init__.py
+++ src/metapensiero/pj/transformations/__init__.py
@@ -35,6 +35,14 @@ def _normalize_dict_keys(transformer, keys):
             if transformer.enable_es6:
                 key = JSKeySubscript(key)
             else:
-                logger.warning('Ambiguous dict key %r and es6 disabled', key)
+                if isinstance(key, ast.AST):
+                    py_node = key
+                elif isinstance(key, TargetNode) and key.py_node is not None:
+                    py_node = key.py_node
+                else:
+                    raise ValueError('Value of type %r cannot '
+                                     'be use as key' % type(key))
+                transformer.unsupported(py_node, True, 'Value of type %r cannot '
+                                        'be use as key' % type(key))
         res.append(key)
     return res
