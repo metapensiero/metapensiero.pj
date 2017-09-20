c4598be1420522ce2733ab4af0325c3626c033f2
Alberto Berti <alberto@metapensiero.it>
2017-09-20 17:15:09 +0200

    Add a test of rendering of a class with a decorated method

---
+++ tests/test_various/test_translate_object/class_with_deco.js
@@ -0,0 +1,32 @@
+var _pj;
+function _pj_snippets(container) {
+    function set_decorators(cls, props) {
+        var deco, decos;
+        var _pj_a = props;
+        for (var p in _pj_a) {
+            if (_pj_a.hasOwnProperty(p)) {
+                decos = props[p];
+                function reducer(val, deco) {
+                    return deco(val, cls, p);
+                }
+                deco = decos.reduce(reducer, cls.prototype[p]);
+                if ((((! ((deco instanceof Function) || (deco instanceof Map) || (deco instanceof WeakMap))) && (deco instanceof Object)) && (("value" in deco) || ("get" in deco)))) {
+                    delete cls.prototype[p];
+                    Object.defineProperty(cls.prototype, p, deco);
+                } else {
+                    cls.prototype[p] = deco;
+                }
+            }
+        }
+    }
+    container["set_decorators"] = set_decorators;
+    return container;
+}
+_pj = {};
+_pj_snippets(_pj);
+class Foo3 {
+    foo() {
+        return "bar";
+    }
+}
+_pj.set_decorators(Foo3, {"foo": [a_deco]});
---
+++ tests/test_various/test_translate_object/class_with_deco.py
@@ -0,0 +1,7 @@
+## enable_es6: True
+
+class Foo3:
+
+    @a_deco
+    def foo(self):
+        return 'bar'
