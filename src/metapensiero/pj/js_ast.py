3b709ef36e2463f8b42eccbb3345bb2f9ebf8c2f
Alberto Berti <alberto@metapensiero.it>
2017-09-20 17:14:48 +0200

    Test if transformer is present

--- src/metapensiero/pj/js_ast.py
+++ src/metapensiero/pj/js_ast.py
@@ -31,10 +31,14 @@ JS_KEYWORDS_ES6 = JS_KEYWORDS - set(['delete'])
 
 def _check_keywords(target_node, name):
     trans = target_node.transformer
-    trans.unsupported(
-        target_node.py_node,
-        name in JS_KEYWORDS_ES6 if trans.enable_es6 else name in JS_KEYWORDS,
-        "Name '%s' is reserved in JavaScript." % name)
+    if trans is not None:
+        trans.unsupported(
+            target_node.py_node,
+            name in JS_KEYWORDS_ES6 if trans.enable_es6 else name in JS_KEYWORDS,
+            "Name '%s' is reserved in JavaScript." % name)
+    else:
+        if name in JS_KEYWORDS:
+            raise ValueError("Name %s is reserved in JavaScript." % name)
 
 #### Misc
 
