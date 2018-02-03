2fb66500fa822b0d07f3942af87cdefd68b66017
Alberto Berti <alberto@metapensiero.it>
2018-02-03 01:11:23 +0100

    Add an export test

---
+++ tests/test_various/test_translate_object/exports.js
@@ -0,0 +1,6 @@
+var bar, foo, zoo;
+foo = 5;
+bar = "boo";
+zoo = 10;
+export {foo, bar, zoo};
+export default zoo;
---
+++ tests/test_various/test_translate_object/exports.py
@@ -0,0 +1,8 @@
+## enable_es6: True
+
+foo = 5
+bar = "boo"
+zoo = 10
+
+__all__ = ('foo', 'bar', 'zoo')
+__default__ = 'zoo'
