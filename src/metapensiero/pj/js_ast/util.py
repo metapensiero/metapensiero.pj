1ee9e137c46afec89009dba6f6839ef434afdc46
Alberto Berti <alberto@metapensiero.it>
2018-03-03 03:22:03 +0100

    Fix ES6 reserved keywords

--- src/metapensiero/pj/js_ast/util.py
+++ src/metapensiero/pj/js_ast/util.py
@@ -10,14 +10,27 @@ JS_KEYWORDS = set([
     'break', 'case', 'catch', 'continue', 'default', 'delete', 'do', 'else',
     'finally', 'for', 'function', 'if', 'in', 'instanceof', 'new', 'return',
     'switch', 'this', 'throw', 'try', 'typeof', 'var', 'void', 'while', 'with',
-
     'abstract', 'boolean', 'byte', 'char', 'class', 'const',
     'double', 'enum', 'export', 'extends', 'final', 'float', 'goto',
     'implements', 'import', 'int', 'interface', 'long', 'native', 'package',
     'private', 'protected', 'public', 'short', 'static', 'super',
     'synchronized', 'throws', 'transient', 'volatile'])
 
-JS_KEYWORDS_ES6 = JS_KEYWORDS - set(['delete'])
+# see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar
+JS_KEYWORDS_ES6 = {
+    'break', 'case', 'catch', 'class', 'const', 'continue', 'debugger',
+    'default', 'delete', 'do', 'else', 'export', 'extends', 'finally', 'for',
+    'function', 'if', 'import', 'in', 'instanceof', 'new', 'return', 'super',
+    'switch', 'this', 'throw', 'try', 'typeof', 'var', 'void', 'while', 'with',
+    'yield'}
+
+# Future keywords
+JS_KEYWORDS_ES6.union({
+    'enum', 'implements', 'interface', 'let', 'package', 'private',
+    'protected', 'public', 'static', 'await'})
+
+# Additional
+JS_KEYWORDS_ES6.union({'null', 'true', 'false'})
 
 
 def _check_keywords(target_node, name):
