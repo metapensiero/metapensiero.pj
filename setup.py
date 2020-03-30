333ee4aeead770260c227067763dfdb5dd3997e5
Alberto Berti <alberto@metapensiero.it>
2020-03-30 02:44:08 +0200

    Update package metadata

--- setup.py
+++ setup.py
@@ -36,6 +36,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         ],
