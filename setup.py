0a66d7d59d4fdd83f2204135e9c7047c7d6c6352
Alberto Berti <alberto@metapensiero.it>
2022-07-18 19:33:24 +0200

    Deprecate Python 3.5 and 3.6 and add 3.10
    It's still compatible with 3.5 and 3.6, but no testing will be done in
    the future

--- setup.py
+++ setup.py
@@ -34,11 +34,10 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         ],
